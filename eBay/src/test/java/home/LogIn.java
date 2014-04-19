f449460be6207b150b7a9f1e3175199c8b285916
Papri-Barua <barua.papri@gmail.com>
2014-04-19 03:24:57 -0400

    adding Class

--- eBay/src/test/java/home/LogIn.java
+++ eBay/src/test/java/home/LogIn.java
@@ -1,9 +1,21 @@
 package home;
 
 import base.Base;
+import org.junit.Test;
 
 /**
  * Created by Prithul on 4/14/2014.
  */
 public class LogIn extends Base {
+
+    @Test
+    public void test() {
+        clickByXpath(".//*[@id='gh-ug']/a");
+        typeByCss("input#userid","prithul28@gmail.com");
+        typeByCss("input#pass","Prithul123");
+        clickByCss("input#sgnBt");
+
+        System.out.println("Successful login in eBay website....:)");
+    }
+
 }
