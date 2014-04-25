46172876b4e233d27bd3e182a429bce56f8cd933
Papri-Barua <barua.papri@gmail.com>
2014-04-24 23:50:34 -0400

    adding Class

---
+++ NYTimes/src/test/java/home/GetLinks.java
@@ -0,0 +1,23 @@
+package home;
+
+import base.Base;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+/**
+ * Created by Prithul on 4/24/2014.
+ */
+public class GetLinks extends Base{
+
+    @Test
+    public void linkTest() {
+        List<WebElement> link = driver.findElements(By.tagName("a"));
+        for(WebElement element:link){
+            System.out.println(element.getText());
+        }
+        System.out.println(link.size());
+    }
+}
